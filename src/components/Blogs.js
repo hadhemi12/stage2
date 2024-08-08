294f8d311de1fac7af99bda88c74910270ad8ae1
hadhemi ben brahim <hadhemibnbrahim@gmail.com>
2024-08-08 01:25:39 +0100

    Initial commit

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import './BlogsStyles.css';
 function Blogs() {
   return (
-    <div>
-      <h2>this is Blogs</h2>
+    <div className='rectangle'>
+     <p className='tit1'>Blogs</p>
+     <hr/>
+     <div className='rectangle1'>
+     <img className='image' src={"https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/oYxKIpgbA7RVnAnCDAA81R8YEbrfKEDEfAfJiJ~tplv-tej9nj120t-origin.webp"} alt='img1'/>
+      <p className='travel'>27 April - Inspiration </p>
+      <p className='chul'>How to Own Your Audience by 
+      <br/>Creating an Email List.</p>
+     </div>
+     <div className='rectangle2'>
+     <img className='image' src={"https://i.pinimg.com/originals/7b/9a/86/7b9a862f7c60a8dac7f7bc69034ee5f1.jpg"} alt='img2'/>
+      <p className='land'>06 october - Web Design </p>
+      <p className='aura'>the window know to 
+      <br/>say beside you</p>
+     </div>
+     <div className='rectangle3'>
+     <img className='image' src={"https://mrwallpaper.com/images/thumbnail/aesthetic-beauty-of-a-beautiful-flower-bhqdkw48kdbe6yue.webp"} alt='img3'/>
+      <p className='land'>12 March - Travel Landing </p>
+      <p className='aura2'>Everything You Need to Know 
+      <br/>About Web Accessibility.</p>
+     </div>
+     <div className='rectangle4'>
+      <img className='image' src={"https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/owDAIgYbV8vEfM7bErRKAAefgABnWAADJAFSiE~tplv-tej9nj120t-origin.webp"} alt='img4'/>
+        <p className='land'>15 november - Web Design </p>
+        <p className='aura3'>Top 10 Toolkits for Deep Learning 
+        <br/>in 2021.</p>
+    </div>
+    <div className='cercle1'></div>
+    <div className='cercle2'></div>
+    <div className='cercle3'></div>
     </div>
   );
 }
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import './WorksStyles.css';
 
 function Works() {
   return (
-    <div>
-      <h2>this is for Works</h2>
+    <div className='rectangle'>
+     <p className='tit1'>Portfolio</p>
+     <hr/>
+     <p className='all'>All</p>
+     <p className='moc'>Mockup</p>
+     <p className='moc2'>Graphic Design</p>
+     <p className='moc3'>logo</p>
+     <div className='rectangle1'>
+      <img className='image1' src={"https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/oYxKIpgbA7RVnAnCDAA81R8YEbrfKEDEfAfJiJ~tplv-tej9nj120t-origin.webp"} alt='img1'/>
+      <p className='travel'>Travel Landing , UX/UI </p>
+      <p className='chul'>Chul urina</p>
+     </div>
+     <div className='rectangle2'>
+      <img className='image2' src={"https://i.pinimg.com/originals/7b/9a/86/7b9a862f7c60a8dac7f7bc69034ee5f1.jpg"} alt='img2'/>
+      <p className='land'>Travel Landing , UX/UI </p>
+      <p className='aura'>Aura Dione</p>
+     </div>
+     <div className='rectangle3'>
+      <img className='image3' src={"https://mrwallpaper.com/images/thumbnail/aesthetic-beauty-of-a-beautiful-flower-bhqdkw48kdbe6yue.webp"} alt='img3'/>
+      <p className='land'>Travel Landing , UX/UI </p>
+      <p className='shirt'>T-Shirt Design</p>
+     </div>
+     <div className='rectangle4'>
+      <img className='image4' src={"https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/owDAIgYbV8vEfM7bErRKAAefgABnWAADJAFSiE~tplv-tej9nj120t-origin.webp"} alt='img4'/>
+      <p className='land'>Travel Landing , UX/UI </p>
+      <p className='shirt'>Packaging Box</p>
+     </div>
+     <div className='rectangle5'>
+      <img className='image5' src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAYwsYdxZpTZpwaROYAUVmCZXGWjk9fBBp3A&usqp=CAU"} alt='img5'/>
+      <p className='land'>Travel Landing , UX/UI </p>
+      <p className='shirt'>Modern Bag Design</p>
+     </div>
+     <div className='rectangle6'>
+      <img className='image6' src={"https://pm1.aminoapps.com/6672/3ba9c1687cfdf17aa3085076d855746ffad161ab_hq.jpg"} alt='img6'/>
+      <p className='land'>Travel Landing , UX/UI </p>
+      <p className='shirt'>Chul urina</p>
+     </div>
+     <div className='footer'>
+      <p className='fin'>© 2021 All Rights Reserved by ib-themes.</p>
+     </div>
+    
     </div>
   );
 }
